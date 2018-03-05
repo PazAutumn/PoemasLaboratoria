4c7118bff5c3a0e958df838b3e3694d88c75b368
Fabián Alexis Bravo Abarca <fabian.bravo.abarca@gmail.com>
2018-03-05 15:40:12 -0300

    VersoForm creado y funcionando

---
+++ src/app/verso-form/verso-form.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-verso-form',
+  templateUrl: './verso-form.component.html',
+  styleUrls: ['./verso-form.component.css']
+})
+export class VersoFormComponent implements OnInit {
+  versoForm = new FormGroup({
+    jugador : new FormControl(),
+    contenido : new FormControl()
+  })
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
